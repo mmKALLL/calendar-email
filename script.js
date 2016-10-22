8a952df1bf392947282fed50e2194a674e844c21
mmKALLL <esakoskin2@gmail.com>
2016-10-22 20:14:28 +0300

    Some cleaning up; divided functions' roles more clearly.

--- script.js
+++ script.js
@@ -6,8 +6,12 @@
 */
 
 function sendEmail(recipient, subject, body, parseIntoHTML) {
-  window.open('mailto:test@example.com?subject=subject&body=as+d%0D%0Afas<br>' +
-    'fasdaf\nasdf</html>');
+  if (parseIntoHTML) {
+    // http://www.w3schools.com/jsref/jsref_replace.asp
+    // Doesn't modify the string in-place.
+  } else {
+    window.open("mailto:" + recipient + "?subject=" + subject + "&body=" + body);
+  }
 }
 
 google.script.run
